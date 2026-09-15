e8691d9c3092f25c290c525c68ee83effb0e5043
_costa <luizfelipeeoliveiraac@gmail.com>
2026-09-14 20:42:05 -0400

    feat(common): add StockImageView component

--- cotla.app/Common/Components/StockImageView.swift
+++ cotla.app/Common/Components/StockImageView.swift
@@ -9,29 +9,45 @@ import UIKit
 import SDWebImage
 
 
-class StockImage: UIImageView {
-    
+class StockImageView: UIImageView {
+
+    var stockImageUrl: String = ""
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        prepareForReuse()
-        configureStockImage()
+
+        setImage(for: stockImageUrl)
     }
-    
+
+    init() {
+        super.init(frame: .zero)
+        configureStockImage()
+        prepareForReuse()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     private func prepareForReuse(){
         self.sd_cancelCurrentImageLoad()
         image = nil
     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
+
     private func configureStockImage() {
-        contentMode = .scaleAspectFit
-        layer.cornerRadius = 10
-        clipsToBounds = true
-        backgroundColor = .tertiarySystemGroupedBackground
-        translatesAutoresizingMaskIntoConstraints = false
+        contentMode                                 = .scaleAspectFit
+        layer.cornerRadius                          = 10
+        clipsToBounds                               = true
+        backgroundColor                             = .tertiarySystemGroupedBackground
+        translatesAutoresizingMaskIntoConstraints   = false
+        layer.cornerRadius                          = 12
+        clipsToBounds                               = true
+    }
+
+    func setImage(for stockImageUrl: String) {
+        sd_setImage(
+            with: URL(string: stockImageUrl),
+            placeholderImage: UIImage(systemName: "chart.bar.fill")
+        )
     }
 }
